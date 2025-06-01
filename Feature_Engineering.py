1c686c5f6d83ab996b02b6f65163e8bed443acf4
Amit Kumar <amitk.pr15@gmail.com>
2025-06-01 22:38:20 +0530

    stop tracking data\processed

---
+++ Feature_Engineering.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import os
+from sklearn.feature_extraction.text import TfidfVectorizer
+import logging
+#Ensure the "logs" directory exists
+log_dir= "logs"
+os.makedirs(log_dir, exist_ok=True)
+
+#Logging Configuration
+logger= logging.getLogger("Feature Engineering")
+logger.setLevel(logging.DEBUG)
+
+console_handler= logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+
+log_file_path= os.path.join(log_dir, "Feature_Engineering.log")
+file_handler= logging.FileHandler(log_file_path)
+file_handler.setLevel(logging.DEBUG)
+
+formatter= logging.Formatter('%(asctime)s- %(name)s -%(levelname)s -%(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+def load_data(file_path:str)->pd.DataFrame:
+    """Load data from csv file"""
+    try:
+        df= pd.read_csv(file_path)
+        df.fillna("", inplace=True)
+        logger.debug("Data loaded and NaN filled from %s", file_path)
+        return df
+    except pd.errors.ParserError as e:
+        logger.error("Failed to parse the csv file : %s", e)
+        raise
+
+def apply_tfidf(train_data :pd.DataFrame, test_data: pd.DataFrame, max_feature: int)->tuple:
+    """Apply TFIDF to the data"""
+    try:
+        vectorizer=TfidfVectorizer(max_features=max_feature)
+        x_train= train_data["Message"].values
+        y_train= train_data["Category"].values
+        x_test= test_data["Message"].values
+        y_test= test_data["Category"].values
+
+        x_train_bow= vectorizer.fit_transform(x_train)
+        x_test_bow= vectorizer.fit_transform(x_test)
+
+        train_df= pd.DataFrame(x_train_bow.toarray())
+        train_df["Category"]= y_train
+
+        test_df= pd.DataFrame(x_test_bow.toarray())
+        test_df["Category"]= y_test
+
+        logger.debug("TF-IDF applied and data transformed")
+
+        return train_df,test_df
+    except Exception as e:
+        logger.error("Error during bag of words transformation %s", e)
+        raise 
+
+def save_data(df: pd.DataFrame, file_path: str)->None:
+    """save the dataframe to a csv file"""
+
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        df.to_csv(file_path, index=False)
+        logger.debug("Data saved to %s", file_path)
+
+    except Exception as e:
+        logger.error("Unexpected error while saving the data %s", e)
+        raise
+
+def main():
+    try:
+        max_feature=50
+        train_data= load_data("E:/git-tutorial/MLOps-OOP/MLOps-Complete-Pipeline/Experiment/src/train_processed.csv")
+        test_data= load_data("E:/git-tutorial/MLOps-OOP/MLOps-Complete-Pipeline/Experiment/src/test_processed.csv")
+
+        train_df, test_df= apply_tfidf(train_data, test_data, max_feature)
+
+        save_data(train_df, os.path.join("./data", "processed", "train_tfidf.csv"))
+        save_data(test_df, os.path.join("./data", "processed", "test_tfidf.csv"))
+
+    except Exception as e:
+        logger.error("Falied to comlete the feature engineering process %s", e)
+        print(f"Error :", e)
+
+if __name__== "__main__":
+    main()
---
+++ data_preprocessing.py
@@ -0,0 +1,115 @@
+import os
+import logging
+import nltk
+import nltk.downloader
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+import string
+nltk.download("stopwords")
+nltk.download("punkt")
+
+# insure the log directory exist
+log_dir= "logs"
+os.makedirs(log_dir, exist_ok=True)
+
+#setting up logger
+
+logger= logging.getLogger("Data Prepocessing")
+logger.setLevel(logging.DEBUG)
+
+console_handler= logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+log_file_path= os.path.join(log_dir, "data_preprocessing.log")
+file_handler= logging.FileHandler(log_file_path)
+file_handler.setLevel(logging.DEBUG)
+
+formatter= logging.Formatter('%(asctime)s- %(name)s -%(levelname)s -%(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+def transform_text(text):
+    """transform the input text into lower case , tokenization, removing stopwords, punctuation and stemming"""
+
+    ps= PorterStemmer()
+    text= text.lower()
+    text= nltk.word_tokenize(text)
+    #remove non alphanumeric tokens
+    text= [word for word in text if word.isalnum()]
+    #remove stopwords and punctuation
+    text= [word for word in text if word not in stopwords.words("english") and word not in string.punctuation]
+    #stem the words
+    text=[ps.stem(word) for word in text]
+    #join the tokens back into the single string
+    return " ".join(text)
+
+def data_preprocess(df, text_column="Message", target_column= "Category"):
+    
+    """Pre-processing the data frame by encoding the target column, removing duplicates and trasnforming the text column"""
+    try:
+        logger.debug("Starting preprocessing the data frame")
+        #encode the target column
+        encoder=LabelEncoder()
+        df["Category"]= encoder.fit_transform(df["Category"])
+        logger.debug("Target column encoded")
+
+        #Remove duplicate rows
+        df =df.drop_duplicates(keep="first")
+        logger.debug("Duplicates removed")
+
+        #Apply text transformation to the specified text column
+        df.loc[:, "Message"]= df["Message"].apply(transform_text)
+        logger.debug("Text column transformed")
+        return df
+   
+    except KeyError as e:
+        logger.error("column not found %s ", e)
+        raise
+    except Exception as e:
+        logger.error("Error during text normalization : %s", e)
+        raise
+
+def main(text_column= "Message", target_column="Category"):
+    """ Main function to load  raw data , process it and save the processed data"""
+
+    try:
+        #Fetch the data from raw
+
+        train_data=pd.read_csv('./data/raw/train.csv')
+        test_data=pd.read_csv("./data/raw/test.csv")
+        logger.debug("Data loaded properly")
+
+        #Transform the data
+
+        train_processed_data=data_preprocess(train_data, text_column, target_column)
+        test_processed_data = data_preprocess(test_data, text_column, target_column)
+
+        # store the data inside the data path
+
+        data_path= os.path.join("./data", "interim")
+        os.makedirs(data_path, exist_ok=True)
+
+        train_processed_data.to_csv(os.path.join(data_path, "train_processed.csv"), index=False)
+        test_processed_data.to_csv(os.path.join(data_path, "test_processed.csv"), index=False)
+        logger.debug("Processed data saved  to %s", data_path)
+
+    except FileNotFoundError as e:
+        logger.error("File not found %s", e)
+    except pd.errors.EmptyDataError as e:
+        logger.error("No data %s", e)
+
+    except Exception as e:
+        logger.error("Failed to complete the data transformation process %s", e)
+        print(f"Error {e}")
+    
+if __name__=="__main__":
+    main()
+
+
+       
+    
